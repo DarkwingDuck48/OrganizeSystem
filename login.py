6b244cd684510e05b10f911375f558f2e3341ed5
Max Britvin <maksbritvin@gmail.com>
2016-10-20 12:45:04 +0300

    Start projectupd

--- login.py
+++ login.py
@@ -17,16 +17,26 @@ class MainWindow(QtWidgets.QMainWindow):
         self.centWidget = QtWidgets.QWidget()
         self.setCentralWidget(self.centWidget)
 
-
+        vbox = QtWidgets.QVBoxLayout()
         hbox = QtWidgets.QHBoxLayout()
-        self.centWidget.setLayout(hbox)
+        hbox_2 = QtWidgets.QHBoxLayout()
+
+        self.centWidget.setLayout(vbox)
 
         self.login = QtWidgets.QLineEdit('Login')
         self.password = QtWidgets.QLineEdit('Password')
+        self.btn_start = QtWidgets.QPushButton('Log in')
 
         hbox.addStretch(1)
         hbox.addWidget(self.login)
         hbox.addWidget(self.password)
+
+        vbox.addLayout(hbox)
+        hbox_2.addSpacing(20)
+        hbox_2.addWidget(self.btn_start)
+        hbox_2.addSpacing(20)
+        vbox.addLayout(hbox_2)
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     ex = MainWindow()
